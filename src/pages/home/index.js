354e7edf417a66b6b5c7232824373b61005444ea
gabrielalegrete <gabrielalegrete1@gmail.com>
2021-07-29 02:55:08 -0300

    remove unecessaries

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Banner from '../../components/banner/index'
 import BestSellers from './bestSellers';
 import SurveySubscription from '../../components/surveySubscription/index'
-import './home.css'
 
 const Home = () => (
     <div>
